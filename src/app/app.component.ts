89e1a24beb51b006f75209600b6900c841c7b1be
loica <loicarchimbaud@gmail.com>
2021-10-06 17:29:20 +0200

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-premier-projet';
+  isAuth :  boolean = false;
+  
+  lastUpdate = new Promise<Date>((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  constructor(){
+    setTimeout( () => {
+      this.isAuth = true;
+    }, 4000
+    ); 
+  }
+  
+  onAllumer(){
+    console.log("On allume tout !");
+    for (let appareil of this.appareils){
+      appareil.status="allumé";
+    }    
+  }
+ 
 }
